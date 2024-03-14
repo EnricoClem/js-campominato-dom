0ea7c8fb1ee5010b0195bd26a26463d8f03ba312
EnricoClem <enrico.clem94@gmail.com>
2024-03-14 12:45:48 +0100

    ADD - random numbers generator within difficulty number of cells range

--- app/app.js
+++ app/app.js
@@ -3,6 +3,16 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l’utente clicca su una cella:
+// se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso.
+// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+// Attenzione che l’utente potrebbe cliccare due volte sulla stessa casella…
+
+
 console.log('Campominato.');
 
 // Collegare con il bottone start
@@ -18,11 +28,11 @@ startGame.addEventListener('click', function () {
     let rowSize = 0;
     let inputDifficultyElement = document.getElementById('difficulty').value;
 
-    if (inputDifficultyElement === "hard") {
+    if (inputDifficultyElement === "easy") {
         rowSize = 10;
     } else if (inputDifficultyElement === "mid") {
         rowSize = 9;
-    } else if (inputDifficultyElement === "easy") {
+    } else if (inputDifficultyElement === "hard") {
         rowSize = 7;
     }
 
@@ -30,6 +40,17 @@ startGame.addEventListener('click', function () {
 
     const numberOfCells = rowSize ** 2;
 
+            // Generare 16 numeri casuali all'interno del range di difficoltà
+            let bombs = [];
+            for (let n = 0; n < 16; ){
+                let randomNumber = Math.floor(Math.random() * numberOfCells);
+                if (!bombs.includes(randomNumber)) {
+                    bombs = bombs.concat(randomNumber);
+                    n++
+                }
+            }
+            console.log(bombs)
+    
     // Generare tante celle all'interno della griglia quante ne richiede il livello di difficoltà selezionato
 
     gridElement.innerHTML = '';
@@ -39,11 +60,11 @@ startGame.addEventListener('click', function () {
 
         let cellElement = document.createElement('div');
 
-        if (inputDifficultyElement === "hard") {
+        if (inputDifficultyElement === "easy") {
             cellElement.classList.add ('cell-10');
         } else if (inputDifficultyElement === "mid") {
             cellElement.className = "cell-9";
-        } else if (inputDifficultyElement === "easy") {
+        } else if (inputDifficultyElement === "hard") {
             cellElement.className = "cell-7";
         }
         
